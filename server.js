274cdd864ac8b51883c6d5fc990a153e16bc1e64
MelroyNoronha <melmax1208@gmail.com>
2017-12-20 21:52:21 +0530

    fixed isuue with port on heroku

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('listening on port 3000!'));
+app.listen(process.env.port || 3000, () => console.log('listening on port 3000!'));
